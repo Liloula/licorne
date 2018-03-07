5d83b5a06bd08a7d137a61d82cea656cd9085dc2
Unknown <eloise.hall@sfr.fr>
2018-03-07 16:55:33 +0100

    ajout

---
+++ ressources/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+$("h1").click(function () {
+$(this).css("background-color", "#ff0000");
+})
+});
+
+$(document).ready(function() {
+$("h2").click(function () {
+$(this).css("background-color", "#ff0000");
+})
+});
