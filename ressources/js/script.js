862b2e2e8da4d57934a85f6d033cb1f988cd30f8
Unknown <eloise.hall@sfr.fr>
2018-03-08 15:08:39 +0100

    ajout ber

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-$("h1").click(function () {
-$(this).css("background-color", "#ff0000");
-})
-});
 
-$(document).ready(function() {
-$("h2").click(function () {
-$(this).css("background-color", "#ff0000");
-})
+  $("h1").click(function () {
+    $(this).css("background-color", "#ff0000");
+  })
+
+    $(".js--waypoint-livre").waypoint(function(direction) {
+      if (direction=="down"){
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    })
+
+    $(".js-scroll-livres").click(function() {
+      $("html, body").animate({scrollTop: $(".js-section-livres").offset().top});
+    })
 });
